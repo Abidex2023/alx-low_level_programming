96b77f60ad034b0ddfc049ea4b9bea7a47db7490
abidex2023 <abidex2023.patrickosiyemi@gmail.com>
2023-02-28 10:17:31 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-int _putchar (char c);
+int _atoi(char *s);
+int _putchar(char c);
 
 #endif
