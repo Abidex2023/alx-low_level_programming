1f60e957425d0eff19264cef1dda55f347bf2555
abidex2023 <abidex2023.patrickosiyemi@gmail.com>
2023-02-28 08:25:08 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 /**
  * _strcpy - copies the string pointed to by src,
@@ -10,14 +10,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int count = 0;
+int count = 0;
 
-	while (count >= 0)
-	{
-		*(dest + count) = *(src + count);
-		if (*(src + count) == '\0')
-			break;
-		count++;
-	}
-	return (dest)
+while (count >= 0)
+{
+*(dest + count) = *(src + count);
+if (*(src + count) == '\0')
+break;
+count++;
+}
+return (dest);
 }
