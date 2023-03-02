39036eab40e59dddc73e9dcaf49ef6be471582ab
abidex2023 <abidex2023.patrickosiyemi@gmail.com>
2023-03-02 00:44:32 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+# include main .h
+/**
+*_isupper- checks if parameter is uppercase character.
+*@c: input charcter.
+*Return 1 if is an uppercase character, 0 in other case.
+*/
+int_isupper(int c)
+{
+ if (c >= 65 & (<= 90)
+       {
+                return (1);
+       }
+       else
+       {
+                return (0);
+       }
+ }
+}
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+
+#if ndef MAIN_H
+#define MAIN_H
+
+int _isupper (int c) ;
+int  _isdigit (int c) ;
+int mul (int a, int b) ;
+void print_numbers (void) ;
+int _putchar(char) ;
+void print _most_number (void) ;
+void more_numbers (void) ;
+void print _line(int n) ;
+void print _diagonal (int n) ;
+void print _square (int size) ;
+void print _ triangle (int size) ;
+
+#endif
